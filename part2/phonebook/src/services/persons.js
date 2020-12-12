99e63f6584c491f67048811a6fe8d9e14f7faa19
Bekar <beqarioni@yahoo.co.uk>
2020-12-12 05:29:05 +0400

    3.11 phonebook full stack

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // const url = 'http://localhost:3001/api/persons'
-const url = 'https://young-sierra-82258.herokuapp.com/api/persons'
+const url = '/api/persons'
 const getAll = () => axios.get(url).then(response=>response.data)
 const create = (newObj) => axios.post(url,newObj).then(r=>r.data)
 const deleteContact = (id) => axios.delete(`${url}/${id}`)
