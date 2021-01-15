4319509b6b9444e37c487ea4c2dece6755f41628
Bekar <beqarioni@yahoo.co.uk>
2021-01-15 05:14:14 +0400

    4.13 Blog list expansions, step1

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -62,6 +62,24 @@ test('a valid blog can be added ', async () => {
         expect(res.body.likes === 0)
       })
   })
+  test('a note can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+  
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd.length).toBe(
+      helper.initialBlogs.length - 1
+    )
+  
+    const titles = blogsAtEnd.map(r => r.title)
+  
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 
 afterAll(() => {
   mongoose.connection.close()
